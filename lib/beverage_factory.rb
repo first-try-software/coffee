faac2e4dbfb8ddebfcd2300b6045eed1124896f4
Alan Ridlehoover <alan@ridlehoover.com>
2022-10-02 19:31:30 -0700

    STORY: As a patron, I want tomato soup
    Co-authored-by: Fito von Zastrow <adolfovon@gmail.com>

--- lib/beverage_factory.rb
+++ lib/beverage_factory.rb
@@ -1,13 +1,15 @@
 require_relative "coffee"
 require_relative "tea"
 require_relative "cocoa"
+require_relative "tomato_soup"
 require_relative "null_beverage"
 
 class BeverageFactory
   BEVERAGES = {
     coffee: Coffee,
     tea: Tea,
-    cocoa: Cocoa
+    cocoa: Cocoa,
+    tomato_soup: TomatoSoup
   }
 
   def self.build(beverage, driver, options)
