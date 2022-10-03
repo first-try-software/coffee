79866bd3591532cc1f20b13a7ab2e8c43ef78b46
Alan Ridlehoover <alan@ridlehoover.com>
2022-10-02 19:31:30 -0700

    STORY: As a patron, I want soup
    Co-authored-by: Fito von Zastrow <adolfovon@gmail.com>

--- lib/beverage_factory.rb
+++ lib/beverage_factory.rb
@@ -2,6 +2,7 @@ require_relative "coffee"
 require_relative "tea"
 require_relative "cocoa"
 require_relative "null_beverage"
+require_relative "soup"
 
 class BeverageFactory
   def self.build(beverage, driver, options)
